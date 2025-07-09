66adabf266deeb583fd25da4ea89b012356fe1c9
Pablo Celis <pablo.celis.projects@gmail.com>
2025-07-09 01:22:54 +0100

    Fix: PostCSS config for Vercel build

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-};
+// postcss.config.js
+import tailwind from '@tailwindcss/postcss'
+import autoprefixer from 'autoprefixer'
+
+export default {
+  plugins: [tailwind(), autoprefixer()],
+}
